f004cfea001f8b7324f565d54930f58f8f3409a9
Sasha <sashguiab@gmail.com>
2018-10-29 15:23:27 -0700

    reverse string

--- src/recursion.js
+++ src/recursion.js
@@ -117,6 +117,7 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  return (string === '') ? '' : reverse(string.substr(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
