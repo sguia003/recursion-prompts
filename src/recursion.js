4944dc37936282b42f1f189f3e0a326579743894
Sasha <sashguiab@gmail.com>
2018-10-29 15:55:47 -0700

    palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -122,6 +122,13 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if(string === '' || string.length === 0){
+    return true;
+  }
+  if(string[0].toLowerCase() !== string.slice(-1).toLowerCase()){
+    return false;
+  }
+  return palindrome(string.substr(1, string.length -2));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
