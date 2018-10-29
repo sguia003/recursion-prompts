4acb8614f1fc983a51422393a3da6850e8e032f2
Sasha <sashguiab@gmail.com>
2018-10-29 13:26:23 -0700

    integer range

--- src/recursion.js
+++ src/recursion.js
@@ -78,6 +78,11 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if(y - x === 1 || y - x === 0){
+    return [];
+  }
+  y = y > x ? y - 1 : y + 1;
+  return x === y ? [] : range(x, y).concat(y);
 };
 
 // 7. Compute the exponent of a number.
